c0fc472aaca8ca0f454201fe1ed8bc2f61d9e751
tabiqchohan <160527853+tabiqchohan@users.noreply.github.com>
2024-03-04 03:57:43 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,36 @@
+// 
+const num7 = 5
+const num8 = 4
+console.log(num7 + num8)
+const num9 = 10
+const num10 = 6
+const sum = num9 + num10
+console.log(sum)
+const num11 = 10
+const num12 = 2
+const sub = num11 - num12
+console.log(sub)
+const num1 = 10
+const num2 = 2
+const mul = num1 * num2
+console.log(mul)
+const num3 = 21
+const num4 = 5
+const mod = num3 % num4 
+console.log(mod)
+const num5 = 5
+const num6 = 2
+const power = num5 ** num6
+console.log(power)
+const firstname = "tabiq"
+const lastname = "chohan"
+console.log(firstname + " " + lastname)
+let n = 5
+n = n + 5
+console.log(n)
+console.log(10==10);
+console.log(10===10);
+console.log(10 != 5)
+console.log(5 > 2);
+console.log(5 < 2);
+console.log(5 <= 5);
